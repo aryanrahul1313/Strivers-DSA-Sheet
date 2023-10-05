d2a198bcc684b1f23013484c4180e2143534ae14
aryanrahul1313 <130580694+aryanrahul1313@users.noreply.github.com>
2023-10-05 15:41:57 +0530

    Create Binary Tree Preorder Traversal.cpp

---
+++ Binary Tree Part-I/Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    void Preorder(TreeNode* root, vector<int> &ans)
+    {
+        if(root == NULL){
+            return;
+        }
+
+        ans.push_back(root->val);
+        Preorder(root->left, ans);
+        Preorder(root->right, ans);
+    }
+    
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        Preorder(root, ans);
+        return ans;
+    }
+};
