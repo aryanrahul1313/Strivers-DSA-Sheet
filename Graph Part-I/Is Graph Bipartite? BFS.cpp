217395e52e0ce97a73eab2c4ea458e205c6fc78f
aryanrahul1313 <130580694+aryanrahul1313@users.noreply.github.com>
2023-10-18 01:05:51 +0530

    Update and rename Is Graph Bipartite?.cpp to Is Graph Bipartite? BFS.cpp

--- Graph Part-I/Is Graph Bipartite? BFS.cpp
+++ Graph Part-I/Is Graph Bipartite? BFS.cpp
@@ -1,4 +1,3 @@
-#Approach 1: BFS
 class Solution {
 private:
     bool BFScheck(int start, vector<vector<int>>& graph, vector<int> &colour)
@@ -36,46 +35,6 @@ public:
         for(int i=0; i<n; i++){
             if(colour[i] == -1){
                 if(BFScheck(i, graph, colour) == false){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-};
-
-*******************************************************************
-#Approach 2 : DFS
-class Solution {
-private:
-    bool dfsCheck(int node, int col, vector<vector<int>>& graph, vector<int> &colour)
-    {
-        colour[node] = col;
-
-        for(auto it : graph[node]){
-            // if not yet coloured
-            if(colour[it] == -1){
-                // recursive call get false, return false
-                if(dfsCheck(it, !col, graph, colour) == false){
-                    return false;
-                }
-            }
-            // adjacent node has same colour
-            else if(colour[it] == col){
-                return false;
-            }
-        }
-        return true;
-    }
-
-public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> colour(n, -1);
-
-        for(int i=0; i<n; i++){
-            if(colour[i] == -1){
-                if(dfsCheck(i, 0, graph, colour) == false){
                     return false;
                 }
             }
