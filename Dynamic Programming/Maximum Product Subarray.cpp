c74bcf47a48eff12daac2f8b771f1eebbaa319b9
aryanrahul1313 <130580694+aryanrahul1313@users.noreply.github.com>
2023-10-24 12:34:07 +0530

    Create Maximum Product Subarray.cpp

---
+++ Dynamic Programming/Maximum Product Subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = INT_MIN;
+        int prefix = 1;
+        int suffix = 1;
+
+        for(int i=0; i<n; i++){
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n-1-i];
+            maxi = max(maxi, max(prefix, suffix));
+        }
+        return maxi;
+    }
+};
